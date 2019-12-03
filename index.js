ac91a44cf6bd66bd9d209ac44244372520ba05b5
Andrey <lazarev.and_18@list.ru>
2019-12-03 09:24:58 +0300

    Terminal ready

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
+/*jshint esversion: 8 */
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const os = require('os');
 const pty = require('node-pty');
+const fetch = require('node-fetch');
 
+//Express & Shell
 const app = express();
+app.use(cookieParser());
 var expressWs = require('express-ws')(app);
-const shell = os.platform() === 'win32' ? `${__dirname}/start.bat` : 'bash';
+const shell = os.platform() === 'win32' ? `cmd.exe` : 'bash';
 
-app.use(express.static(`${__dirname}/static`));
+//Firebase Admin
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-vortex.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://vortex-4b6db.firebaseio.com"
+});
 
+//Open websocket
 expressWs.app.ws('/shell', (ws, req) => {
+    //Verify user
+    admin.auth().verifyIdToken(req.cookies.access_token).then(async (claims) => {
+        //Get sshUserId arg
+        let sshUserId = req.query.sshUserId;
 
+        if(sshUserId){
+            //Request sshConnectionString from core
+            let sshConnectionString = await fetch(`http://localhost:5000/service/GetSshConnecton?sshUserId=${sshUserId}&uid=${claims.user_id}`).then(res => res.json());
+
+            // Spawn the shell
+            const ptyProcess = pty.spawn(shell, ['/k', 'ssh', sshConnectionString], {
+                name: 'xterm-color',
+                cwd: process.env.PWD,
+                env: process.env
+            });
+            // For all shell data send it to the websocket
+            ptyProcess.on('data', (data) => {
+                ws.send(data);
+            });
+            // For all websocket data send it to the shell
+            ws.on('message', (msg) => {
+                ptyProcess.write(msg);
+            });
+        }
+        else{
+            ws.close();
+        }
+    });
     //Heartbeat
     let pingTimer = setInterval(() => {
         if(ws.readyState == 3) {
-            clearInterval(pingTimer)
-            return
+            clearInterval(pingTimer);
+            return;
         }
         else{
             ws.ping("heartbeat");
         }
     }, 500);
 
-    // Spawn the shell
-    const ptyProcess = pty.spawn(shell, [], {
-      name: 'xterm-color',
-      cwd: process.env.PWD,
-      env: process.env
-    });
-    // For all shell data send it to the websocket
-    ptyProcess.on('data', (data) => {
-      ws.send(data);
-    });
-    // For all websocket data send it to the shell
-    ws.on('message', (msg) => {
-        ptyProcess.write(msg);
-    });
 });
 
 // Start the application
